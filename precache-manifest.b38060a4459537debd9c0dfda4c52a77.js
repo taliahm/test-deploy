292784abfada965048bd02a888b51d6c234101f1
Talia <hm.talia@gmail.com>
2020-09-25 07:58:54 -0700

    Updates

---
+++ precache-manifest.b38060a4459537debd9c0dfda4c52a77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "701768db2493aa882500228d9c74d94e",
+    "url": "/test-deploy/index.html"
+  },
+  {
+    "revision": "e0e3704ddd7d983da415",
+    "url": "/test-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "de4550b11f222c188dbe",
+    "url": "/test-deploy/static/js/2.9dfde261.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-deploy/static/js/2.9dfde261.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0e3704ddd7d983da415",
+    "url": "/test-deploy/static/js/main.672125a9.chunk.js"
+  },
+  {
+    "revision": "10a40f72d75b5dd0db7b",
+    "url": "/test-deploy/static/js/runtime-main.96d093a3.js"
+  }
+]);
